ec4c55cbfdf6638a184ec3064e6deffde93322cd
df5602 <dominikf@ee.ethz.ch>
2016-10-16 16:33:56 +0200

    test

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 {
+                continue 'outer;
+            }
+            if y % 2 == 0 {
+                continue 'inner;
+            }
+            println!("x: {}, y: {}", x, y);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {}
